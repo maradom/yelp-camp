e02a753a6f230e9b7475a4d357659086960e8560
Martynas Adomaitis <MartynasAdomaitis90@gmail.com>
2021-04-26 17:27:36 +0100

    Introducing authentication, authorization, restructuring, controllers

--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "6082b9c05ceecb2f80e91c35",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
